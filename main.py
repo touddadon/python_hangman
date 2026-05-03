cd238e339afee66338338b0b28d49d171b7e34e1
sonya <satishchenko@edu.hse.ru>
2026-05-03 19:50:53 +0300

    ASCII hangman drawings

--- main.py
+++ main.py
@@ -1,19 +1,7 @@
 from random import randint
 from time import sleep
 from re import search
-
-def print_hangman(stage=0):
-    hangman = [
-        "  +--+",
-        "  |  O",
-        "  | /|\\",
-        "  | / \\",
-        "  |",
-        "--+-----"
-    ]
-    if stage > 0:
-        print('\n'.join(hangman[0:stage]))
-    print(" \n"*(6-stage))
+from json import load
 
 def print_and_clear(output='', clear_screen=True, sleep_time=1):
     if output is not None:
@@ -21,10 +9,10 @@ def print_and_clear(output='', clear_screen=True, sleep_time=1):
         sleep(sleep_time)
         print("\033[F\r\033[K", end='')
     if clear_screen is True:
-        print("\033[F\033[K"*9, end='')
+        print("\033[H\033[J", end='')
     return
 
-def play_hangman(words):
+def play_hangman(words, hangman_stages):
     GAME_IN_PROGRESS = False
     while (True):
         if not GAME_IN_PROGRESS:
@@ -35,25 +23,24 @@ def play_hangman(words):
                 GAME_IN_PROGRESS = True
                 true_word = words[randint(0, len(words)-1)]
                 letter = '-'
-                mistakes_count = 0
                 player_won = False
-                hangman_stage = 0
+                mistakes_count = 0
                 hidden_word = ['_', '_', '_', '_', '_', '_']
                 used_letters = []
             print('\033[F\033[K', end='')
 
-        print_hangman(hangman_stage)
+        print('\n'.join(hangman_stages[mistakes_count]))
         print(''.join(hidden_word))
 
         player_won = (true_word == ''.join(hidden_word))
 
         print(f"Всего ошибок: {mistakes_count}. Последняя введенная буква: {letter}. Введите букву русского алфавита: ", end='')
-        if hangman_stage < 6 and not(player_won):
+        if mistakes_count < 6 and not(player_won):
             letter = input()
         else:
             print()
 
-        if hangman_stage == 6:
+        if mistakes_count == 6:
             print_and_clear(f"Вы проиграли! Правильное слово: {true_word}", sleep_time=2)
             GAME_IN_PROGRESS = False
             continue
@@ -79,16 +66,17 @@ def play_hangman(words):
                 if letter == true_word[i]:
                     hidden_word[i] = letter
         else:
-            hangman_stage += 1
-            print_and_clear("Такой буквы нет в загаданном слове.", clear_screen=False)
             mistakes_count += 1
+            print_and_clear("Такой буквы нет в загаданном слове.", clear_screen=False)
 
         print_and_clear()
 
 if __name__ == "__main__":
     with open('words.txt', 'r', encoding='utf-8') as f:
         words = [line.strip().lower() for line in f if len(line.strip()) == 6]
+    with open('hangman_stages.json', 'r', encoding='utf-8') as f:
+        hangman_stages = load(f)
     if not words:
         print("Файл со словами пуст.")
     else:
-        play_hangman(words)
+        play_hangman(words, hangman_stages)
