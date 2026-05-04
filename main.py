40bc4a4883b782b30317d9b4e0904edcd9a4f0c3
sonya <satishchenko@edu.hse.ru>
2026-05-05 02:50:58 +0300

    refactor: make separate function for game screen

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 from json import load
-from random import randint
+from random import choice
 from re import search
 from time import sleep
 from typing import Tuple
 
 
 def print_and_clear(
-    output: str = "", clear_screen: bool = True, sleep_time: int = 1
+    output: str, clear_screen: bool = True, sleep_time: int = 1
 ) -> None:
     if output is not None:
         print(output)
@@ -51,7 +51,7 @@ def check_game_over(
 def player_turn(
     true_word: str, mistakes_count: int, hidden_word: list[str], used_letters: list[str]
 ) -> Tuple[str, int]:
-    letter: str = input()
+    letter: str = input().strip().lower()
     if check_hangman_entry(letter, used_letters):
         used_letters.append(letter)
         if letter in true_word:
@@ -61,8 +61,20 @@ def player_turn(
         else:
             mistakes_count += 1
             print_and_clear("Такой буквы нет в загаданном слове.", clear_screen=False)
-        print_and_clear()
+        print_and_clear("")
     return letter, mistakes_count
+
+
+def print_game_screen(
+    hangman_stages: list[list[str]],
+    mistakes_count: int,
+    hidden_word: list[str],
+    letter: str,
+):
+    print("\n".join(hangman_stages[mistakes_count]))
+    print("".join(hidden_word))
+    print(f"Всего ошибок: {mistakes_count}. Последний ввод: {letter}.")
+    print("Введите букву русского алфавита: ", end="")
 
 
 def game_cycle(
@@ -75,11 +87,7 @@ def game_cycle(
     game_in_progress: bool = True
     letter: str = "-"
     while game_in_progress:
-        print("\n".join(hangman_stages[mistakes_count]))
-        print("".join(hidden_word))
-        print(f"Всего ошибок: {mistakes_count}. Последний ввод: {letter}.")
-        print("Введите букву русского алфавита: ", end="")
-
+        print_game_screen(hangman_stages, mistakes_count, hidden_word, letter)
         if check_game_over(mistakes_count, true_word, hidden_word):
             game_in_progress = False
         else:
@@ -90,16 +98,16 @@ def game_cycle(
 
 def init_newgame_or_exit(words: list[str], hangman_stages: list[list[str]]) -> None:
     while True:
-        choice: str = input("Начать новую игру или выйти? (newgame/exit) ")
-        if choice == "newgame":
-            true_word: str = words[randint(0, len(words) - 1)]
+        player_entry: str = input("Начать новую игру или выйти? (newgame/exit) ")
+        if player_entry == "newgame":
+            true_word: str = choice(words)
             mistakes_count: int = 0
             hidden_word: list[str] = ["_"] * len(true_word)
             used_letters: list[str] = []
             game_cycle(
                 true_word, mistakes_count, hidden_word, used_letters, hangman_stages
             )
-        elif choice == "exit":
+        elif player_entry == "exit":
             return
         else:
             print_and_clear("Неправильный ввод.")
@@ -110,8 +118,10 @@ if __name__ == "__main__":
         words_file: list[str] = [
             line.strip().lower() for line in f if line.strip().isalpha()
         ]
+
     with open("hangman_stages.json", "r", encoding="utf-8") as f:
         hangman_stages_file: list[list[str]] = load(f)
+
     if not words_file or not hangman_stages_file:
         print("words.txt или hangman_stages.json пуст.")
     else:
